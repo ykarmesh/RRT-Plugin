ee696d79b1b6e9de773b559f269fe850381fa697
Karmesh Yadav <ykarmesh@gmail.com>
2017-09-02 16:49:33 +0530

    Before subscribing to mapserver again

--- src/OptimalPlanning.cpp
+++ src/OptimalPlanning.cpp
@@ -73,10 +73,7 @@ ros::Publisher vis_pub;
 int i=0;
 /// @cond IGNORE
 boost::shared_ptr<const nav_msgs::OccupancyGrid> costmap;
-// Our "collision checker". For this demo, our robot's state space
-// lies in [0,1]x[0,1], with a circular obstacle of radius 0.25
-// centered at (0.5,0.5). Any states lying in this circular region are
-// considered "in collision".
+// Our "collision checker".
 class ValidityChecker : public ob::StateValidityChecker
 {
 public:
@@ -98,7 +95,7 @@ public:
         // example, so we downcast state into the specific type.
         const auto* state2D =
             state->as<ob::RealVectorStateSpace::StateType>();
-        std::cout<<" "<<costmap->getCost(1,1)<<"blah"<<"   ";
+        //std::cout<<" "<<costmap->getCost(1,1)<<"blah"<<"   ";
         // Extract the robot's (x,y) position from its state
         double x = state2D->values[0];
         double y = state2D->values[1];
@@ -120,8 +117,228 @@ public:
     }
 };
 
+class Plan{
+public:
+    Plan(costmap_2d::Costmap2DROS* cmap)
+    { ros::NodeHandle private_nh("~");
+      ros::Subscriber goal_sub = private_nh.subscribe<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1, &Plan::goalCb);
+      cmap_ = cmap;
+    }
 
-void visualize(const og::PathGeometric* path);
+    void initrrt()
+    {
+      space = ob::StateSpacePtr(new ob::RealVectorStateSpace(2));
+      // set the bounds
+      ob::RealVectorBounds bounds(2);
+      bounds.setLow(0, costmap->info.origin.position.x);
+      bounds.setHigh(0, costmap->info.origin.position.x + costmap->info.width * costmap->info.resolution);
+      bounds.setLow(1, costmap->info.origin.position.y);
+      bounds.setHigh(1, costmap->info.origin.position.y + costmap->info.height * costmap->info.resolution);
+      space->setBounds(bounds);
+      //std::cout<< costmap->info.origin.position.x + costmap->info.width * costmap->info.resolution<<" "<<costmap->info.origin.position.y + costmap->info.height * costmap->info.resolution<<std::endl;
+      // Construct a space information instance for this state space
+
+      si = ob::SpaceInformationPtr(new ob::SpaceInformation(space));
+
+      // Set the object used to check which states in the space are valid
+      si->setStateValidityChecker(std::make_shared<ValidityChecker>(si));
+
+      si->setup();
+
+      // Set our robot's starting state to be the bottom-left corner of
+      // the environment, or (0,0).
+      ob::ScopedState<> start(space);
+      start->as<ob::RealVectorStateSpace::StateType>()->values[0] = 0.0;
+      start->as<ob::RealVectorStateSpace::StateType>()->values[1] = 0.0;
+
+      // Set our robot's goal state to be the top-right corner of the
+      // environment, or (1,1).
+      ob::ScopedState<> goal(space);
+      goal->as<ob::RealVectorStateSpace::StateType>()->values[0] = 0.0;
+      goal->as<ob::RealVectorStateSpace::StateType>()->values[1] = 0.0;
+
+      // Create a problem instance
+      pdef = ob::ProblemDefinitionPtr(new ob::ProblemDefinition(si));
+
+      // Set the start and goal states
+      pdef->setStartAndGoalStates(start, goal);
+      //std::cout<<"second"<<std::endl;
+      // Create the optimization objective specified by our command-line argument.
+      // This helper function is simply a switch statement.
+      pdef->setOptimizationObjective(getPathLengthObjective(si));
+
+      std::cout<<"initialized";
+    }
+
+    void setGoal(double x, double y)
+    {
+        if(prev_goal[0] != x || prev_goal[1] != y )
+        {
+            ob::ScopedState<> goal(space);
+            start->as<ob::RealVectorStateSpace::StateType>()->values[0] = x;
+            start->as<ob::RealVectorStateSpace::StateType>()->values[1] = y;
+            prev_goal[0] = x;
+            prev_goal[1] = y;
+            pdef->clearGoal();
+			      pdef->setGoalState(goal);
+            std::cout << "Goal point set to: " << x << " " << y << std::endl;
+        }
+    }
+
+    void setStart(double x, double y)
+    {
+        ob::ScopedState<> start(space);
+        start->as<ob::RealVectorStateSpace::StateType>()->values[0] = x;
+        start->as<ob::RealVectorStateSpace::StateType>()->values[1] = y;
+        pdef->clearStartStates();
+        pdef->addStartState(start);
+    }
+
+    void rrtplan(void)
+    {
+        // Construct the optimal planner specified by our command line argument.
+        // This helper function is simply a switch statement.
+        ob::PlannerPtr optimizingPlanner = std::make_shared<og::RRTstar>(si);
+
+        // Set the problem instance for our planner to solve
+        optimizingPlanner->setProblemDefinition(pdef);
+        optimizingPlanner->setup();
+
+        // print the settings for this space
+		    si->printSettings(std::cout);
+
+	      // print the problem settings
+		    pdef->print(std::cout);
+
+	       // attempt to solve the problem within one second of planning time
+		    ob::PlannerStatus solved = rrtplan->solve(0.05);
+
+        if (solved)
+        {
+            // Output the length of the path found
+            std::cout
+                << optimizingPlanner->getName()
+                << " found a solution of length "
+                << pdef->getSolutionPath()->length()
+                << " with an optimization objective value of "
+                << pdef->getSolutionPath()->cost(pdef->getOptimizationObjective()) << std::endl;
+
+            // If a filename was specified, output the path as a matrix to
+            // that file for visualization
+            if (!outputFile.empty())
+            {
+                std::ofstream outFile(outputFile.c_str());
+                std::static_pointer_cast<og::PathGeometric>(pdef->getSolutionPath())->
+                    printAsMatrix(outFile);
+                outFile.close();
+
+                path = new og::PathGeometric(dynamic_cast<const og::PathGeometric&>(*pdef->getSolutionPath()));
+                path->print(std::cout);
+                visualize(path);
+          }
+      }
+      else
+          std::cout << "No solution found." << std::endl;
+    }
+
+    void visualize(void)
+    {
+        ros::Duration(3).sleep();
+        visualization_msgs::Marker marker, line;
+
+        for (std::size_t idx = 0; idx < path->getStateCount (); idx++)
+        {
+
+          // cast the abstract state type to the type we expect
+          const auto *rstate = static_cast<const ob::RealVectorStateSpace::StateType *>(path->getState(idx));
+          const ob::SE3StateSpace::StateType *se3state = path->getState(idx)->as<ob::SE3StateSpace::StateType>();
+
+          // extract the second component of the state and cast it to what we expect
+          const auto *qstate = static_cast<const ob::SO3StateSpace::StateType *>(path->getState(idx));
+
+          marker.header.frame_id = line.header.frame_id = "map";
+          marker.header.stamp = line.header.stamp = ros::Time::now();
+          marker.ns = "path";
+          line.ns = "pathline";
+          marker.id = idx;
+          line.id = idx;
+          marker.type  = visualization_msgs::Marker::CUBE;
+          line.type = visualization_msgs::Marker::LINE_STRIP;
+          marker.action = line.action = visualization_msgs::Marker::ADD;
+          geometry_msgs::Point p;
+          marker.pose.position.x = rstate->values[0];
+          marker.pose.position.y = rstate->values[1];
+          marker.pose.position.z = 0;
+          p.x = rstate->values[0];
+          p.y = rstate->values[1];
+          p.z = 0;
+          marker.points.push_back(p);
+          line.points.push_back(p);
+          //std::cout<<"iteration "<<idx<<std::endl;
+          marker.pose.orientation.x = qstate->x;
+          marker.pose.orientation.y = qstate->y;
+          marker.pose.orientation.z = qstate->z;
+          marker.pose.orientation.w = qstate->w;
+          line.pose.orientation.w = 1;
+          marker.scale.x = 0.2;
+          marker.scale.y = 0.2;
+          marker.scale.z = 0.2;
+          line.scale.x = 0.1;
+          line.scale.y = 0.1;
+          line.scale.z = 0.4;
+          marker.color.a = 1.0;
+          marker.color.r = 0.0;
+          marker.color.g = 1.0;
+          marker.color.b = 0.0;
+          line.color.a = 0.5;
+          line.color.r = 0.0;
+          line.color.g = 0.0;
+          line.color.b = 1.0;
+          vis_pub.publish(marker);
+          vis_pub.publish(line);
+          std::cout << "Published marker: " << idx << std::endl;
+      }
+    }
+
+    void goalCb(const geometry_msgs::PoseStamped::ConstPtr &goal)
+    {
+    	  setGoal(goal->point.x, goal->point.y);
+          //set start state
+        tf::Stamped<tf::Pose> global_pose;
+        cmap_->getRobotPose(global_pose);
+
+        setStart(global_pose.getOrigin().x(),global_pose.getOrigin().y());
+        //geometry_msgs::PoseStamped start;
+        //start.header.stamp = global_pose.stamp_;
+        //start.header.frame_id = global_pose.frame_id_;
+        //start.pose.position.x = global_pose.getOrigin().x();
+        //start.pose.position.y = global_pose.getOrigin().y();
+        //start.pose.position.z = global_pose.getOrigin().z();
+        //start.pose.orientation.x = global_pose.getRotation().x();
+        //start.pose.orientation.y = global_pose.getRotation().y();
+        //start.pose.orientation.z = global_pose.getRotation().z();
+        //start.pose.orientation.w = global_pose.getRotation().w();
+    }
+
+private:
+
+    ob::StateSpacePtr space;
+
+    // construct an instance of  space information from this state space
+    ob::SpaceInformationPtr si;
+
+    // create a problem instance
+    ob::ProblemDefinitionPtr pdef;
+
+    double prev_goal[2];
+
+    const og::PathGeometric* path;
+
+    std::string outputFile("outputfile");
+
+    std::shared_ptr<costmap_2d::Costmap2DROS> cmap_;
+}
+
 
 ob::OptimizationObjectivePtr getPathLengthObjective(const ob::SpaceInformationPtr& si);
 /*
@@ -135,169 +352,31 @@ ob::OptimizationObjectivePtr getBalancedObjective2(const ob::SpaceInformationPtr
 
 ob::OptimizationObjectivePtr getPathLengthObjWithCostToGo(const ob::SpaceInformationPtr& si);
 */
-void costmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg, const std::string& outputFile)
+
+void mapserverCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg, Plan* planptr)
 {
-     // construct the state space we are planning in
-     auto space(std::make_shared<ob::RealVectorStateSpace>(2));
-
-     //costmap = dynamic_cast<nav_msgs::OccupancyGrid*>(msg);
-     costmap = msg;
-     // set the bounds
-     //std::cout<<"first"<<std::endl;
-     ob::RealVectorBounds bounds(2);
-     bounds.setLow(0, msg->info.origin.position.x);
-     bounds.setHigh(0, msg->info.origin.position.x + msg->info.width * msg->info.resolution);
-     bounds.setLow(1, msg->info.origin.position.y);
-     bounds.setHigh(1, msg->info.origin.position.y + msg->info.height * msg->info.resolution);
-     space->setBounds(bounds);
-     std::cout<< msg->info.origin.position.x + msg->info.width * msg->info.resolution<<" "<<msg->info.origin.position.y + msg->info.height * msg->info.resolution<<std::endl;
-     // Construct a space information instance for this state space
-     auto si(std::make_shared<ob::SpaceInformation>(space));
-
-     // Set the object used to check which states in the space are valid
-     si->setStateValidityChecker(std::make_shared<ValidityChecker>(si));
-     si->setStateValidityCheckingResolution(0.0005);
-
-     si->setup();
-
-     // Set our robot's starting state to be the bottom-left corner of
-     // the environment, or (0,0).
-     ob::ScopedState<> start(space);
-     start->as<ob::RealVectorStateSpace::StateType>()->values[0] = 1.0;
-     start->as<ob::RealVectorStateSpace::StateType>()->values[1] = 1.0;
-
-     // Set our robot's goal state to be the top-right corner of the
-     // environment, or (1,1).
-     ob::ScopedState<> goal(space);
-     goal->as<ob::RealVectorStateSpace::StateType>()->values[0] = 8.0;
-     goal->as<ob::RealVectorStateSpace::StateType>()->values[1] = 8.0;
-
-     // Create a problem instance
-     auto pdef(std::make_shared<ob::ProblemDefinition>(si));
-
-     // Set the start and goal states
-     pdef->setStartAndGoalStates(start, goal);
-     //std::cout<<"second"<<std::endl;
-     // Create the optimization objective specified by our command-line argument.
-     // This helper function is simply a switch statement.
-     pdef->setOptimizationObjective(getPathLengthObjective(si));
-
-     // Construct the optimal planner specified by our command line argument.
-     // This helper function is simply a switch statement.
-     ob::PlannerPtr optimizingPlanner = std::make_shared<og::RRTstar>(si);
-
-     // Set the problem instance for our planner to solve
-     optimizingPlanner->setProblemDefinition(pdef);
-     optimizingPlanner->setup();
-
-     //std::cout<<"third"<<std::endl;
-
-     // attempt to solve the planning problem in the given runtime
-     ob::PlannerStatus solved = optimizingPlanner->solve(0.05);
-
-     //std::cout<<"fourth"<<std::endl;
-
-     if (solved)
-     {
-         // Output the length of the path found
-         std::cout
-             << optimizingPlanner->getName()
-             << " found a solution of length "
-             << pdef->getSolutionPath()->length()
-             << " with an optimization objective value of "
-             << pdef->getSolutionPath()->cost(pdef->getOptimizationObjective()) << std::endl;
-
-         // If a filename was specified, output the path as a matrix to
-         // that file for visualization
-         if (!outputFile.empty())
-         {
-             std::ofstream outFile(outputFile.c_str());
-             std::static_pointer_cast<og::PathGeometric>(pdef->getSolutionPath())->
-                 printAsMatrix(outFile);
-             outFile.close();
-
-             const og::PathGeometric* path = new og::PathGeometric(dynamic_cast<const og::PathGeometric&>(*pdef->getSolutionPath()));
-             path->print(std::cout);
-             visualize(path);
-         }
-     }
-     else
-         std::cout << "No solution found." << std::endl;
-}
-
-void visualize(const og::PathGeometric* path)
-{
-    ros::Duration(3).sleep();
-    visualization_msgs::Marker marker, line;
-
-    for (std::size_t idx = 0; idx < path->getStateCount (); idx++)
-    {
-
-      // cast the abstract state type to the type we expect
-      const auto *rstate = static_cast<const ob::RealVectorStateSpace::StateType *>(path->getState(idx));
-      const ob::SE3StateSpace::StateType *se3state = path->getState(idx)->as<ob::SE3StateSpace::StateType>();
-
-      // extract the second component of the state and cast it to what we expect
-      const auto *qstate = static_cast<const ob::SO3StateSpace::StateType *>(path->getState(idx));
-
-      marker.header.frame_id = line.header.frame_id = "map";
-      marker.header.stamp = line.header.stamp = ros::Time::now();
-      marker.ns = "path";
-      line.ns = "pathline";
-      marker.id = idx;
-      line.id = idx;
-      marker.type  = visualization_msgs::Marker::CUBE;
-      line.type = visualization_msgs::Marker::LINE_STRIP;
-      marker.action = line.action = visualization_msgs::Marker::ADD;
-      geometry_msgs::Point p;
-      marker.pose.position.x = rstate->values[0];
-      marker.pose.position.y = rstate->values[1];
-      marker.pose.position.z = 0;
-      p.x = rstate->values[0];
-      p.y = rstate->values[1];
-      p.z = 0;
-      marker.points.push_back(p);
-      line.points.push_back(p);
-      //std::cout<<"iteration "<<idx<<std::endl;
-      marker.pose.orientation.x = qstate->x;
-      marker.pose.orientation.y = qstate->y;
-      marker.pose.orientation.z = qstate->z;
-      marker.pose.orientation.w = qstate->w;
-      line.pose.orientation.w = 1;
-      marker.scale.x = 0.2;
-      marker.scale.y = 0.2;
-      marker.scale.z = 0.2;
-      line.scale.x = 0.1;
-      line.scale.y = 0.1;
-      line.scale.z = 0.4;
-      marker.color.a = 1.0;
-      marker.color.r = 0.0;
-      marker.color.g = 1.0;
-      marker.color.b = 0.0;
-      line.color.a = 0.5;
-      line.color.r = 0.0;
-      line.color.g = 0.0;
-      line.color.b = 1.0;
-      vis_pub.publish(marker);
-      vis_pub.publish(line);
-      std::cout << "Published marker: " << idx << std::endl;
-  }
+    ros::Duration(1).sleep();
+	  planptr->initrrt();
 }
 
 int main(int argc, char** argv)
-{
-    // The parsed arguments
-    ros::init(argc, argv,"optimal_planning_node");
-    ros::NodeHandle nh;
-    std::string outputFile("outputfile");
-    //std::cout << "main";
+{   ros::init(argc, argv,"optimal_planning_node");
+
     ompl::msg::setLogLevel(ompl::msg::LOG_DEBUG);
 
-  //  costmap_ros = new costmap_2d::Costmap2DROS("costmap", tf_);
+    ros::NodeHandle nh;
 
-    ros::Subscriber map_sub = nh.subscribe<nav_msgs::OccupancyGrid>("/map",1,boost::bind(&costmapCallback, _1, outputFile));
+    tf::TransformListener tf(ros::Duration(10));
+
+    costmap_2d::Costmap2DROS costmap("global_costmap",tf);
+
+    ros::Subscriber map_sub = nh.subscribe<nav_msgs::OccupancyGrid>("/map", 1, &mapserverCallback);
+
     vis_pub = nh.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
-    ros::spin();
+
+    Plan planner_object(&costmap);
+
+    ros::spin());
     return 0;
 }
 
